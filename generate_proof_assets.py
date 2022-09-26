77cf79ac27f362a38b3e15f2f649083897513717
onsen-egg <work-account-root@protonmail.com>
2022-09-26 21:29:42 +0000

    Correct improper transaction hash (sha256, not sha512_256) in proof asset generation

--- generate_proof_assets.py
+++ generate_proof_assets.py
@@ -2,17 +2,22 @@ import algosdk
 import os
 import sys
 from base64 import b32decode, b64decode
+from block_fetcher import decode_block
+from hashlib import sha256
 
-# In CLI run: python3 generate_proof_assets.py [FOLDER_NAME]
 
-def generate_proof_assets(folder_name):
-  token = ""
-  net = "https://testnet-api.algonode.cloud"
+# In CLI run: python3 generate_proof_assets.py <FOLDER_NAME> <NETWORK>
+# <NETWORK> is optional, can be mainnet or testnet
+
+def generate_proof_assets(folder_name, network='testnet'):
+  token = ''
+  net = 'https://{}-api.algonode.cloud'.format(network)
 
   client = algosdk.v2client.algod.AlgodClient(token, net)
-  idx_net = "https://testnet-idx.algonode.cloud"
+  idx_net = 'https://{}-idx.algonode.cloud'.format(network)
   indexer = algosdk.v2client.indexer.IndexerClient(token, idx_net)
 
+  client_old = algosdk.algod.AlgodClient(token, net)
   if os.path.exists(folder_name):
     raise Exception('The folder name {} is already in use'.format(folder_name))
 
@@ -22,15 +27,18 @@ def generate_proof_assets(folder_name):
   status = client.status()
   round_num = status['last-round'] - 512 
 
-  txns_for_round = indexer.search_transactions(limit=10, round_num=round_num)
+  state_proof = client.stateproofs(round_num)
 
-  tx_id = txns_for_round['transactions'][0]['id']
+  block_txns = decode_block(client.block_info(round_num, response_format='msgpack'))
+  txn_to_validate = block_txns[0].txn
 
-  state_proof = client.stateproofs(round_num) 
-  first_round = state_proof['Message']['FirstAttestedRound']
+  txn_bytes = b64decode(algosdk.encoding.msgpack_encode(txn_to_validate.transaction))
+  hasher = sha256()
+  hasher.update(b'TX' + txn_bytes)
+  tx_raw_hash = hasher.digest()
 
   lightblockheader_proof = client.lightblockheader_proof(round_num)
-  transaction_proof = client.transaction_proof(round_num, tx_id, hashtype='sha256')
+  transaction_proof = client.transaction_proof(round_num, txn_to_validate.get_txid(), hashtype='sha256')
   block_info = client.block_info(round_num)
   prev_block_info = client.block_info(round_num - 1)
 
@@ -45,7 +53,7 @@ def generate_proof_assets(folder_name):
   tx_assets['light_block_header_proof_response.json'] = str(lightblockheader_proof).replace('\'', '\"').replace(' ', '')
   tx_assets['round.txt'] = str(round_num)
   tx_assets['seed.txt'] = str(list(b64decode(block_info['block']['seed']))).replace(' ', '')
-  tx_assets['transaction_id.txt'] = str(list(b32decode(tx_id + '===='))).replace(' ', '')
+  tx_assets['transaction_id.txt'] = str(list(tx_raw_hash)).replace(' ', '')
   tx_assets['transaction_proof_response.json'] = str(transaction_proof).replace('\'', '\"').replace(' ', '')
 
   for pair in zip(
@@ -60,7 +68,8 @@ def generate_proof_assets(folder_name):
         k), 'w') as f:
         f.write(str(pair[0][k]))
 
-
 if __name__ == '__main__':
-  generate_proof_assets(sys.argv[1])
-  
+  if len(sys.argv) > 2:
+    generate_proof_assets(sys.argv[1], sys.argv[2])
+  else:
+    generate_proof_assets(sys.argv[1])
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import sys
 from utils import AssetLoader, verify_transaction
 
-asset_folder = 'testing'
+# In CLI run: python3 main.py <ASSET_FOLDER_NAME>
 
-assets = AssetLoader(asset_folder)
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+    raise Exception('Provide a folder containing the proof assets as an argument')
 
-result = verify_transaction(
-  assets.tx_hash,
-  assets.tx_proof,
-  assets.lightblockheader_proof,
-  assets.round_number,
-  assets.genesis_hash,
-  assets.seed,
-  assets.block_interval_commitment
-)
+  assets = AssetLoader(sys.argv[1])
 
-assert(result)
+  result = verify_transaction(
+    assets.tx_hash,
+    assets.tx_proof,
+    assets.lightblockheader_proof,
+    assets.round_number,
+    assets.genesis_hash,
+    assets.seed,
+    assets.block_interval_commitment
+  )
+
+  assert(result)
